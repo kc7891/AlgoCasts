9b94d60b81faebe3f63c863ef6a45306f4189df6
Kento Chiba <c.kento.chiba@gmail.com>
2019-08-11 15:46:37 +0900

    Complete capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,13 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  splitedStr = str.split(' ')
+
+  return splitedStr.reduce(( result, word ) => {
+    const capitalizedWord = `${word.slice(0,1).toUpperCase()}${word.slice(1)}`
+    return `${result} ${capitalizedWord}`
+  },'').trim()
+}
 
 module.exports = capitalize;
