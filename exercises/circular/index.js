cd4eb93ad552b7e6fd3cf5e8b1b2fe764019800e
Kento Chiba <c.kento.chiba@gmail.com>
2019-08-12 21:22:18 +0900

    Complete circular

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,17 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+
+  let node = list.head
+  const existingNodes = []
+
+  while ( node && node.next ) {
+    if ( existingNodes.includes(node) ) return true
+    existingNodes.push(node)
+    node = node.next
+  }
+  return false
+}
 
 module.exports = circular;
