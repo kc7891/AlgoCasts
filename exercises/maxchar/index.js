594d735cc60358000abcf7a31af18f4da1031780
Kento Chiba <c.kento.chiba@gmail.com>
2019-08-16 23:57:25 +0900

    Complete maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const summary = str.split('').reduce((accumulator,currentValue)=>{
+    let charCount = accumulator[ currentValue ]
+    charCount = charCount ? charCount + 1 : 1
+    accumulator[ currentValue ] = charCount
+    return accumulator
+  },{})
+
+  return Object.keys(summary).reduce( ( previousKey, key ) => {
+    return summary[previousKey] > summary[key] ? previousKey : key
+  } )
+
+}
 
 module.exports = maxChar;
