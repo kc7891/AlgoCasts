a842a70cd940f1ee35fe20abb0c88e15308603d4
Kento Chiba <c.kento.chiba@gmail.com>
2019-08-23 09:11:37 +0900

    draft

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,86 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    // create empty
+    const result = []
+    for ( let i = 0; i < n; i++ ) {
+        result.push([])
+    }
+
+    const directions = {
+        0: { target: y, distance: -1 },
+        1: { target: x, distance: 1 },
+        2: { target: y, distance: 1 },
+        3: { target: x, distance: -1 },
+    }
+    const position = { x: -1, y: 0 }
+    const step = n
+    const count = 1
+    for ( let i = 1; i <= n; i++ ) {
+        const direction = directions[i%4]
+        const nextPosition = position[direction.target] + direction.distance
+        position[ direction.target ] = nextPosition
+    }
+}
+
+// function matrix(n) {
+    
+//     // create empty
+//     const result = []
+//     for ( let i = 0; i < n; i++ ) {
+//         result.push([])
+//     }
+
+//     let positionX = -1
+//     let positionY = 0
+//     let previousDirection = 'right'
+//     for ( let i = 1; i <= n * n; i++ ) {
+//         const directions = {
+//             right: { x: positionX + 1 , y: positionY},
+//             down: { x: positionX , y: positionY + 1 },
+//             left: { x: positionX - 1 , y: positionY },
+//             up: { x: positionX , y: positionY -1 },
+//         }
+//         const right = { x: positionX + 1 , y: positionY}
+//         const down  = { x: positionX , y: positionY + 1 }
+//         const left  = { x: positionX - 1 , y: positionY }
+//         const up    = { x: positionX , y: positionY -1 }
+
+//         const sameAsPrevious = directions[previousDirection]
+//         if ( sameAsPrevious.x < n 
+//             && sameAsPrevious.x >= 0 
+//             && sameAsPrevious.y < n 
+//             && sameAsPrevious.y >= 0
+//             && !result[sameAsPrevious.y][sameAsPrevious.x] ) {
+//             direction = sameAsPrevious
+
+//         } else if ( right.x < n && !result[right.y][right.x] ) {
+//             direction = right
+//             previousDirection = 'right'
+//         } else if ( down.y < n && !result[down.y][down.x] ) {
+//             direction = down
+//             previousDirection = 'down'
+//         } else if ( left.x >= 0 && !result[left.y][left.x] ) {
+//             direction = left
+//             previousDirection = 'left'
+//         } else if ( up.y >= 0 && !result[up.y][up.x] ) {
+//             direction = up
+//             previousDirection = 'up'
+//         }
+
+//         positionX = direction.x
+//         positionY = direction.y
+
+//         console.log({i,positionX,positionY})
+
+//         result[positionY][positionX] = i
+
+//     }
+
+//     console.log(result)
+
+//     return result
+// }
 
 module.exports = matrix;
